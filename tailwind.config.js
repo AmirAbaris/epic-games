0b54cea27c70171053c1a04b7daa66516732faec
Abaris <118467205+AmirAbaris@users.noreply.github.com>
2024-02-10 16:21:29 +0330

    [#2-set up] added tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
-  theme: {
-    extend: {},
+export const content = [
+  "./src/**/*.{html,ts}",
+];
+export const theme = {
+  extend: {
+    colors: {
+      darkTransparent: '#00000080',
+      darkGray: '#0b0b0b',
+      darkSlate: '#101014',
+      charcoal: '#1c1c1c',
+      darkCharcoal: '#202020',
+      darkSilver: '#252525',
+      vividBlue: '#0074e4',
+      electricBlue: '#037aee',
+      brightBlue: '#0078f2',
+      lightGray: '#f8f8f8'
+    }
   },
-  plugins: [],
-}
+};
+export const plugins = [];
 
