4a545b1dbe86b99aa2964285cb708798ea275264
Abaris <118467205+AmirAbaris@users.noreply.github.com>
2024-02-15 16:22:02 +0330

    [#31-small highlight card] done

--- src/app/components/home/highlight-game-card/highlight-game-card.component.ts
+++ src/app/components/home/highlight-game-card/highlight-game-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, EventEmitter, Output, input } from '@angular/core';
 import { HomeMainCaptionModel } from '../models/caption-models/home-main-captions.model';
 import { HighlightGamesModel } from '../models/highlight-games-model';
 
@@ -12,5 +12,19 @@ export class HighlightGameCardComponent {
   gameInputs = input.required<HighlightGamesModel>();
   currentGameIndexInput = input.required<number>();
   captionInputs = input.required<HomeMainCaptionModel>();
+
+  @Output('playButton') playButtonEvent = new EventEmitter();
+  @Output('addWishlistButton') AddToWishListButtonEvent = new EventEmitter();
+  //#endregion
+
+
+  //#region handler methods
+  public onPlayButtonEvent(): void {
+    this.playButtonEvent.emit();
+  }
+
+  public onAddToWishlistButtonEvent(): void {
+    this.playButtonEvent.emit();
+  }
   //#endregion
 }
