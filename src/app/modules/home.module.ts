1f24b237adc552ede9f75d8af05027361d045101
Abaris <118467205+AmirAbaris@users.noreply.github.com>
2024-02-20 01:19:47 +0330

    [#55-image implement] added image component

--- src/app/modules/home.module.ts
+++ src/app/modules/home.module.ts
@@ -22,6 +22,7 @@ import { GameItemListComponent } from '../components/home/game-item-list/game-it
 import { GameListManagementComponent } from '../components/home/game-list-management/game-list-management.component';
 import { GameListComponent } from '../components/home/game-list/game-list.component';
 import { MatDividerModule } from '@angular/material/divider';
+import { HomeImageComponent } from '../components/home/home-image/home-image.component';
 
 const homeRoutes: Routes = [
   { path: '', component: HomeMainComponent }
@@ -41,7 +42,8 @@ const homeRoutes: Routes = [
     FortniteCardComponent,
     GameListManagementComponent,
     GameListComponent,
-    GameItemListComponent
+    GameItemListComponent,
+    HomeImageComponent
   ],
   imports: [
     CommonModule,
