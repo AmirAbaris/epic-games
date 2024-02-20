e755c78e3e21925fbc0d17bf3b0e6dc37e4120b3
Abaris <118467205+AmirAbaris@users.noreply.github.com>
2024-02-20 22:22:50 +0330

    [#61-animation logic fix] trying to fix animaiton index target

--- src/app/components/home/highlight-game-card/highlight-game-card.component.ts
+++ src/app/components/home/highlight-game-card/highlight-game-card.component.ts
@@ -6,27 +6,7 @@ import { trigger, state, style, transition, animate, keyframes } from '@angular/
 @Component({
   selector: 'app-highlight-game-card',
   templateUrl: './highlight-game-card.component.html',
-  styleUrl: './highlight-game-card.component.scss',
-  animations: [
-    trigger('cardAnimation', [
-      state('inactive', style({
-        background: 'transparent'
-      })),
-      state('active', style({
-        background: 'linear-gradient(to right, darkGray 0%, darkGray 50%, transparent 50%, transparent 100%)',
-        backgroundSize: '200% 100%'
-      })),
-      transition('inactive => active', [
-        animate('1s')
-      ]),
-      transition('active => inactive', [
-        animate('0.5s', keyframes([
-          style({ backgroundPosition: '0% 0%', offset: 0 }), // Start with background on the left
-          style({ backgroundPosition: '100% 0%', offset: 1 })
-        ]))
-      ])
-    ])
-  ]
+  styleUrl: './highlight-game-card.component.scss'
 })
 export class HighlightGameCardComponent {
   //#region properties
