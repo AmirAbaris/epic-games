13eee163ba23024309838d021238280060940ce0
Abaris <118467205+AmirAbaris@users.noreply.github.com>
2024-02-20 21:29:17 +0330

    [#60-logic implementation] more fixes for logic and animation

--- src/app/components/home/highlight-game-card/highlight-game-card.component.ts
+++ src/app/components/home/highlight-game-card/highlight-game-card.component.ts
@@ -1,11 +1,32 @@
-import { Component, EventEmitter, Output, computed, input } from '@angular/core';
+import { Component, EventEmitter, Output, input } from '@angular/core';
 import { HomeMainCaptionModel } from '../models/caption-models/home-main-captions.model';
 import { HighlightGamesModel } from '../models/highlight-games-model';
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-highlight-game-card',
   templateUrl: './highlight-game-card.component.html',
-  styleUrl: './highlight-game-card.component.scss'
+  styleUrl: './highlight-game-card.component.scss',
+  animations: [
+    trigger('cardAnimation', [
+      state('inactive', style({
+        background: 'transparent'
+      })),
+      state('active', style({
+        background: 'linear-gradient(to right, darkGray 0%, darkGray 50%, transparent 50%, transparent 100%)',
+        backgroundSize: '200% 100%'
+      })),
+      transition('inactive => active', [
+        animate('1s')
+      ]),
+      transition('active => inactive', [
+        animate('0.5s', keyframes([
+          style({ backgroundPosition: '0% 0%', offset: 0 }), // Start with background on the left
+          style({ backgroundPosition: '100% 0%', offset: 1 })
+        ]))
+      ])
+    ])
+  ]
 })
 export class HighlightGameCardComponent {
   //#region properties
@@ -15,7 +36,6 @@ export class HighlightGameCardComponent {
   @Output('playButton') playButtonEvent = new EventEmitter();
   @Output('addWishlistButton') AddToWishListButtonEvent = new EventEmitter();
   //#endregion
-
 
   //#region handler methods
   public onPlayButtonEvent(): void {
--- src/app/components/home/small-highlight-game/small-highlight-game.component.ts
+++ src/app/components/home/small-highlight-game/small-highlight-game.component.ts
@@ -5,27 +5,7 @@ import { animate, keyframes, state, style, transition, trigger } from '@angular/
 @Component({
   selector: 'app-small-highlight-game',
   templateUrl: './small-highlight-game.component.html',
-  styleUrl: './small-highlight-game.component.scss',
-  animations: [
-    trigger('cardAnimation', [
-      state('inactive', style({
-        background: 'transparent'
-      })),
-      state('active', style({
-        background: 'linear-gradient(to right, darkGray 0%, darkGray 50%, transparent 50%, transparent 100%)',
-        backgroundSize: '200% 100%'
-      })),
-      transition('inactive => active', [
-        animate('1s')
-      ]),
-      transition('active => inactive', [
-        animate('0.5s', keyframes([
-          style({ backgroundPosition: '0% 0%', offset: 0 }), // Start with background on the left
-          style({ backgroundPosition: '100% 0%', offset: 1 })
-        ]))
-      ])
-    ])
-  ]
+  styleUrl: './small-highlight-game.component.scss'
 })
 export class SmallHighlightGameComponent {
   //#region properties
