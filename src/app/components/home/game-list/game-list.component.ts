f900abac4c45b9f32134843d554a30a05657fb2d
Abaris <118467205+AmirAbaris@users.noreply.github.com>
2024-02-25 20:31:42 +0330

    [#83-overall fixes]

--- src/app/components/home/game-list/game-list.component.ts
+++ src/app/components/home/game-list/game-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, OnInit, input } from '@angular/core';
 import { GameListItemModel } from '../models/game-list-item.model';
 import { GameListItemDto } from '../dots/game-list-item-dto';
 import { GameCategoryEnum } from '../enums/category-types.enum';
@@ -8,9 +8,27 @@ import { GameCategoryEnum } from '../enums/category-types.enum';
   templateUrl: './game-list.component.html',
   styleUrl: './game-list.component.scss'
 })
-export class GameListComponent {
+export class GameListComponent implements OnInit {
   //#region properties
   gameInput = input.required<GameListItemDto[]>();
-  public gameCategories: GameCategoryEnum | undefined;
+  public gameCategories: GameCategoryEnum[] = [GameCategoryEnum.TopSellers, GameCategoryEnum.MostPlayed, GameCategoryEnum.TopUpcomingWishlisted];
+
+  public firstGameCategory: GameListItemDto[] | undefined;
+  public secondGameCategory: GameListItemDto[] | undefined;
+  public thirdGameCategory: GameListItemDto[] | undefined;
+  //#endregion
+
+  //#region lifecycle methods
+  ngOnInit(): void {
+    this.filterGames(this.gameInput());
+  }
+  //#endregion
+
+  //#region main logic methods
+  filterGames(games: GameListItemDto[]): void {
+    this.firstGameCategory = games.filter((game) => game.categoryType === this.gameCategories[0]);
+    this.secondGameCategory = games.filter((game) => game.categoryType === this.gameCategories[1]);
+    this.thirdGameCategory = games.filter((game) => game.categoryType === this.gameCategories[2]);
+  }
   //#endregion
 }
