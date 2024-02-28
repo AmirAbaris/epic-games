3072b727c20a1aa7f37e923901ba20b8a9bbc319
Abaris <118467205+AmirAbaris@users.noreply.github.com>
2024-02-29 00:06:15 +0330

    [#93-animation] added loading animation

--- src/app/components/home/game-item-list/game-item-list.component.ts
+++ src/app/components/home/game-item-list/game-item-list.component.ts
@@ -1,13 +1,31 @@
 import { Component, EventEmitter, OnInit, Output, input } from '@angular/core';
 import { GameListItemModel } from '../models/game-list-item.model';
 import { GameListItemDto } from '../dtos/game-list-item-dto';
+import {interval, take} from "rxjs";
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-game-item-list',
   templateUrl: './game-item-list.component.html',
-  styleUrl: './game-item-list.component.scss'
+  styleUrl: './game-item-list.component.scss',
+  animations: [
+    trigger('fadeInOut', [
+      state('loading', style({
+        opacity: 1
+      })),
+      state('loaded', style({
+        opacity: 0
+      })),
+      transition('loading => loaded', [
+        animate('0.3s')
+      ]),
+      transition('loaded => loading', [
+        animate('0.3s')
+      ])
+    ])
+  ]
 })
-export class GameItemListComponent {
+export class GameItemListComponent implements OnInit{
   //#region properties
   gameInput = input.required<GameListItemDto>();
 
@@ -15,7 +33,19 @@ export class GameItemListComponent {
   @Output() clickItemEvent = new EventEmitter<string>();
 
   public showWishlist: boolean = false;
+
+  // mocked loading
+  public isLoading: boolean = true;
   //#endregion
+
+  //region Lifecycle methods
+  ngOnInit() {
+    // mock loading until main logic refactor
+    interval(5000).pipe(take(1)).subscribe(() => {
+      this.isLoading = !this.isLoading;
+    })
+  }
+  //endregion
 
   //region Handler methods
   onClickWishlistButton(gameId: string) {
