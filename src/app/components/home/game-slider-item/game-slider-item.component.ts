d242934acbfa8a82367c22ae66f90ce0c1eb4f1f
Abaris <118467205+AmirAbaris@users.noreply.github.com>
2024-03-12 16:09:39 +0330

    [#145-UPDATE] updated ng

--- src/app/components/home/game-slider-item/game-slider-item.component.ts
+++ src/app/components/home/game-slider-item/game-slider-item.component.ts
@@ -5,60 +5,60 @@ import {PriceLabelModel} from "../models/price-label.model";
 import {GameSliderCaptionModel} from "../models/caption-models/game-slider-caption.model";
 
 @Component({
-  selector: 'app-game-slider-item',
-  templateUrl: './game-slider-item.component.html',
-  styleUrl: './game-slider-item.component.scss'
+    selector: 'app-game-slider-item',
+    templateUrl: './game-slider-item.component.html',
+    styleUrl: './game-slider-item.component.scss'
 })
 export class GameSliderItemComponent implements OnInit {
-  //region Properties
-  data = input.required<GameSliderItemInputModel>();
-  caption = input.required<GameSliderCaptionModel>();
-  isLoading = input.required<boolean>();
+    //region Properties
+    data = input.required<GameSliderItemInputModel>();
+    caption = input.required<GameSliderCaptionModel>();
+    isLoading = input.required<boolean>();
 
-  @Output() clickItemEvent = new EventEmitter<string>();
-  @Output() clickWishlistButtonEvent = new EventEmitter<string>();
+    @Output() clickItemEvent = new EventEmitter<string>();
+    @Output() clickWishlistButtonEvent = new EventEmitter<string>();
 
-  public readonly SizeEnum = SizeEnum;
-  public priceLabelData: PriceLabelModel | undefined;
+    public readonly SizeEnum = SizeEnum;
+    public priceLabelData: PriceLabelModel | undefined;
 
-  //endregion
+    //endregion
 
-  //region Lifecycle methods
-  ngOnInit(): void {
-    this._setPriceLabelData();
-  }
-
-  //endregion
-
-  //region Main logic methods
-  private _setPriceLabelData(): void {
-    this.priceLabelData = this._convertGameSliderItemInputModelToPriceLabelModel(this.data());
-  }
-
-  //endregion
-
-  //region Handler methods
-  public onClickItemEventHandler(id: string): void {
-    this.clickItemEvent.emit(id);
-  }
-
-  public onClickWishlistButtonEventHandler(event: MouseEvent, id: string): void {
-    // Prevent event propagation
-    event.stopPropagation();
-
-    this.clickWishlistButtonEvent.emit(id);
-  }
-
-  //endregion
-
-  //region Helper methods
-  private _convertGameSliderItemInputModelToPriceLabelModel(gameSliderInput: GameSliderItemInputModel): PriceLabelModel {
-    return {
-      discountPercent: gameSliderInput.discountPercent,
-      basePrice: gameSliderInput.basePrice,
-      finalPrice: gameSliderInput.finalPrice
+    //region Lifecycle methods
+    ngOnInit(): void {
+        this._setPriceLabelData();
     }
-  }
 
-  //endregion
+    //endregion
+
+    //region Main logic methods
+    private _setPriceLabelData(): void {
+        this.priceLabelData = this._convertGameSliderItemInputModelToPriceLabelModel(this.data());
+    }
+
+    //endregion
+
+    //region Handler methods
+    public onClickItemEventHandler(id: string): void {
+        this.clickItemEvent.emit(id);
+    }
+
+    public onClickWishlistButtonEventHandler(event: MouseEvent, id: string): void {
+        // Prevent event propagation
+        event.stopPropagation();
+
+        this.clickWishlistButtonEvent.emit(id);
+    }
+
+    //endregion
+
+    //region Helper methods
+    private _convertGameSliderItemInputModelToPriceLabelModel(gameSliderInput: GameSliderItemInputModel): PriceLabelModel {
+        return {
+            discountPercent: gameSliderInput.discountPercent,
+            basePrice: gameSliderInput.basePrice,
+            finalPrice: gameSliderInput.finalPrice
+        }
+    }
+
+    //endregion
 }
