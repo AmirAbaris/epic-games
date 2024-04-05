eec5d3885fcfe2be4eeab3bf6b88070add5399b1
Abaris <118467205+AmirAbaris@users.noreply.github.com>
2024-04-06 02:33:22 +0330

    [#178-FEATURE] added loading animation and output for button type component

--- src/app/components/home/helpers/highlight-button-type/highlight-button-type.component.ts
+++ src/app/components/home/helpers/highlight-button-type/highlight-button-type.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, EventEmitter, Output, input } from '@angular/core';
 import { HighlightButtonEnum } from '../../enums/highlight-button.enum';
 import { HighlightButtonTypeEnumCaptionModel } from '../../models/caption-models/highlight-button-type-caption.model';
 
@@ -12,5 +12,14 @@ export class HighlightButtonTypeComponent {
   buttonTypeEnumInput = input.required<HighlightButtonEnum>();
   caption = input.required<HighlightButtonTypeEnumCaptionModel>();
   public buttonTypeEnum: typeof HighlightButtonEnum = HighlightButtonEnum;
+
+  @Output() clickItemButtonEvent = new EventEmitter<void>();
+  //#endregion
+
+  //#region Handler methods
+  public onClickItemButtonEventHandler(): void {
+    this.clickItemButtonEvent.emit();
+    console.log('clicked');
+  }
   //#endregion
 }
