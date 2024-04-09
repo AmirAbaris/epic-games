40de18446b9e20cabedbc8aef33e82ed9cc333ea
Abaris <118467205+AmirAbaris@users.noreply.github.com>
2024-04-09 14:37:03 +0330

    [#180-FEATURE] customizing wishlist button

--- src/app/components/home/highlight-preview-item/highlight-preview-item.component.ts
+++ src/app/components/home/highlight-preview-item/highlight-preview-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output, input } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output, input, model } from '@angular/core';
 import { HighlightPreviewItemInputModel } from '../models/highlight-preview-item-input.model';
 import { HighlightButtonEnum } from '../enums/highlight-button.enum';
 import { HighlightButtonTypeEnumCaptionModel } from '../models/caption-models/highlight-button-type-caption.model';
@@ -12,10 +12,10 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
   animations: [
     trigger('spin-icon', [
       state('rotate', style({
-        transform: 'rotate(180deg)'
+        transform: 'rotate(-360deg)',
       })),
       transition('* => rotate', [
-        animate('0s')
+        animate('1000ms')
       ])
     ])
   ]
@@ -24,7 +24,8 @@ export class HighlightPreviewItemComponent {
   //#region Properties
   data = input.required<HighlightPreviewItemInputModel>();
   isLoading = input.required<boolean>();
-  isAddToWishlistLoading = input.required<boolean>();
+  isAddToWishlistLoading = false;
+  isInWishlist = false;
   caption = input.required<HighlightPreviewItemCaptionModel>();
   highlightButtonTypeCaptionData = input.required<HighlightButtonTypeEnumCaptionModel>();
   public buttonTypeEnum: typeof HighlightButtonEnum = HighlightButtonEnum;
@@ -40,6 +41,15 @@ export class HighlightPreviewItemComponent {
 
   public onClickItemEvent(): void {
     this.clickItemEvent.emit();
+  }
+
+  public addToWishlistHandler(): void {
+    this.isAddToWishlistLoading = true;
+
+    setTimeout(() => {
+      this.isAddToWishlistLoading = false;
+      this.isInWishlist = true;
+    }, 2000);
   }
   //#endregion
 }
