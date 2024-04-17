55208f2a0d26ec5b15c01b67720b94cd6637dd38
Abaris <118467205+AmirAbaris@users.noreply.github.com>
2024-04-17 12:34:03 +0330

    [#91-FEATURE] implement highlight small item

---
+++ src/app/components/home/models/highlight-small-item-input.model.ts
@@ -0,0 +1,4 @@
+export interface HighlightSmallItemInputModel {
+    cover: string;
+    name: string;
+}
---
+++ src/app/directives/scale-on-activate.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ScaleOnActivateDirective } from './scale-on-activate.directive';
+
+describe('ScaleOnActivateDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ScaleOnActivateDirective();
+    expect(directive).toBeTruthy();
+  });
+});
