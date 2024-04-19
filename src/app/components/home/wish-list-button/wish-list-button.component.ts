f4dfa109531dbd78da8829d11f812e1396def721
Abaris <118467205+AmirAbaris@users.noreply.github.com>
2024-04-20 01:43:10 +0330

    [#206-UPDATE] created new wishlist for the prev component and used old wishlist for view mobile view

--- src/app/components/home/wish-list-button/wish-list-button.component.ts
+++ src/app/components/home/wish-list-button/wish-list-button.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Output, inject } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
+import { TranslateService } from "@ngx-translate/core";
 
 @Component({
   selector: 'app-wish-list-button',
@@ -9,17 +9,16 @@ import { TranslateService } from '@ngx-translate/core';
 export class WishListButtonComponent {
   //#region Properties
   private _translateService = inject(TranslateService);
-  
-  @Output() clickButtonEvent = new EventEmitter();
 
-  public tooltipMessage: string = this._translateService.instant(
-    'home.WishListButton.addTitle',
-  );
+  @Output() clickButtonEvent = new EventEmitter<void>();
+
+  public tooltipMessage: string = this._translateService.instant('home.WishListButton.addTitle');
   //#endregion
 
   //#region Handler methods
   public onClickButtonEventHandler(): void {
     this.clickButtonEvent.emit();
+    console.log(this.tooltipMessage);
   }
   //#endregion
 }
