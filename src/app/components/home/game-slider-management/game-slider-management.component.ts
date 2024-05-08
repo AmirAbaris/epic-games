fbb06fbb11e48d9a0c3e7172da8d39abae7258c5
Abaris <118467205+AmirAbaris@users.noreply.github.com>
2024-05-08 16:54:57 +0330

    [#127-FEATURE] implemented Swiper and its logic

--- src/app/components/home/game-slider-management/game-slider-management.component.ts
+++ src/app/components/home/game-slider-management/game-slider-management.component.ts
@@ -1,14 +1,14 @@
-import { AfterContentInit, AfterViewInit, Component, input, output } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, input, output, viewChild } from '@angular/core';
 import { GameSliderItemInputModel } from "../models/game-slider-item-input.model";
 import { GameSliderCaptionModel } from "../models/caption-models/game-slider-caption.model";
-import { Swiper } from 'swiper';
+import { SwiperOptions } from 'swiper/types';
 
 @Component({
     selector: 'app-game-slider-management',
     templateUrl: './game-slider-management.component.html',
     styleUrl: './game-slider-management.component.scss'
 })
-export class GameSliderManagementComponent implements AfterContentInit {
+export class GameSliderManagementComponent implements AfterViewInit {
     //#region Properties
     gameCardInputs = input.required<GameSliderItemInputModel[]>();
     isLoading = input.required<boolean>();
@@ -21,16 +21,21 @@ export class GameSliderManagementComponent implements AfterContentInit {
     clickItemEventHandler = output<string>();
     clickWishlistButtonEvent = output<string>();
     clickTitleEvent = output<string>();
+
+    swiper = viewChild.required<ElementRef<HTMLElement>>('swiper');
+
+    private readonly _swiperConfig: SwiperOptions = {
+        slidesPerView: 5,
+        navigation: {
+            nextEl: '.next-btn',
+            prevEl: '.prev-btn',
+        }
+    };
     //#endregion
 
     //#region Lifecycle methods
-    ngAfterContentInit(): void {
-        const swiper = new Swiper('.swiper-container', {
-            navigation: {
-                nextEl: '.next-btn',
-                prevEl: '.prev-btn',
-            }
-        });
+    ngAfterViewInit(): void {
+        this._initSwiperElement(this.swiper(), this._swiperConfig);
     }
     //#endregion
 
@@ -47,6 +52,23 @@ export class GameSliderManagementComponent implements AfterContentInit {
         if (this.isTitleClickable()) {
             this.clickTitleEvent.emit(title);
         }
+    }
+    //#endregion
+
+    //#region Main logic methods
+    /**
+ * Initializes the Swiper element with the provided configuration options.
+ * @param element The ElementRef of the HTMLElement representing the Swiper container.
+ * @param config The configuration options for the Swiper.
+ */
+    private _initSwiperElement(element: ElementRef<HTMLElement>, config: SwiperOptions): void {
+        // Assign configuration options to the Swiper element
+        Object.assign(element.nativeElement, config);
+
+        // Initialize the Swiper element (this is a side effect of using Swiper elements)
+        // Note: Using @ts-ignore is not a recommended practice, but it's necessary in this case due to Swiper's implementation.
+        // @ts-ignore
+        element.nativeElement.initialize();
     }
     //#endregion
 }
